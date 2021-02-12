7ead9afb5f250437886d7ff63a1323c53db2f910
marlenescobarl <marlenescobarlima11@gmail.com>
2021-02-11 22:12:06 -0600

    se agrego archivo de estructura condicional WHILE

---
+++ V13bucleswhile.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class V13bucleswhile
+{
+    public static void main (String args[])
+    {
+        Scanner go = new Scanner (System.in);
+
+        int dato = 0, condicion = 0, b = 0;
+
+        System.out.println("ESTRUCTURA CONDICIONAL WHILE");
+        System.out.println("");
+        System.out.println("Ingrese el numero a iniciar");
+        dato = go.nextInt();
+        System.out.println("Ingrese la condicion ");
+        condicion = go.nextInt();
+        System.out.println("Ingrese el salto ");
+        b = go.nextInt();
+
+        while (dato < condicion)
+        {
+            System.out.print( dato + " , ");
+            dato += b;
+        }
+
+
+
+    }
+}
