16e916151d594e648b14d2aa31ae30d2a517e0ab
marlenescobarl <marlenescobarlima11@gmail.com>
2021-02-21 19:48:12 -0600

    Se agregó el ciclo par WHILE

--- V15ejercicios.java
+++ V15ejercicios.java
@@ -14,13 +14,33 @@ public class V15ejercicios
             //instruccion
             if (i < 10)
             {
-                System.out.print(i + ","); // imprimira en pantala el valor de i y despues la coma
+                System.out.print(i + ", "); // imprimira en pantala el valor de i y despues la coma
             } 
             else 
             {
-                System.out.print(i ""); // se condiciona que al ser menor que 10 despues de, ya no se imprimira la ,
+                System.out.print(i); // se condiciona que al ser menor que 10 despues de, ya no se imprimira la ,
             }
 
+        }
+        //se iguala la varibale a 1 nuevamnte para que vuelva a iniciar el ciclo
+        System.out.println("");
+        i = 1;
+        System.out.println("Ciclo con while");
+
+        //impresion en pantalla de: 1,2,3,4,5,6,7,8,9,10
+        // se da de una vez la condicion
+        while( i <= 10)
+        {   
+            //si es igual a 10, entonces imprime  seguido de coma
+            if ( i < 10)
+            {
+                System.out.print(i + ", ");
+            }
+            else // de lo contrario imprime lo siguiente sin coma
+            {
+                System.out.print(i); 
+            }
+            i ++ ; // para cerrar el ciclo, el incremento
         }
     }
 }
