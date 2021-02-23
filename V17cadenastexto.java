dcf80b590977a19daea17eb73761da48924082dc
marlenescobarl <marlenescobarlima11@gmail.com>
2021-02-22 20:23:34 -0600

    se agregó el archivo para operar comparaciones de cadena de texto con el metodo equals ();

---
+++ V17cadenastexto.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class V17cadenastexto 
+{
+    public static void main (String args[])
+    {
+        /*
+        Realizar un programa que compare dos nombres para verificar si son iguales
+        y mostrar un mensaje dependientemente del resultado. ingreso de datos
+        usando el metodo de comparación de canedas de texto 
+        equals();
+         */
+         //     objeto
+         String nombre_uno = "", nombre_dos = "";
+         Scanner entrada = new Scanner (System.in);
+
+         System.out.print("Ingrese el primer nombre: ");
+         nombre_uno = entrada.nextLine();
+
+         System.out.println("");
+
+         System.out.print("Ingrese el segundo nombre")
+         nombre_dos = entrada.nextLine();
+
+         //condicion para hacer la comparaaion de los textos
+
+
+    }
+}
