2c0c8b2d7dd749abe1eaebe8f7c308c7c554537b
marlenescobarl <marlenescobarlima11@gmail.com>
2021-02-26 22:00:21 -0600

    se creo el vector de 5 espacios

--- V19vectores.java
+++ V19vectores.java
@@ -2,9 +2,33 @@ public class V19vectores
 {
     public static void main(String args [])
     {
-        
+
         System.out.println("Arreglos undimensionales CON VECTORES");
         System.out.println("");
+
+        // creacion de vector tipo entero
+
+        int numeros[] = new int [5]; // nombre del vector: numeros, tamano del vectot: 5 posiciones;
+
+        //la posicion inicial siempre sera cero
+        numeros [0] = 1;
+        numeros [1] = 90;
+        numeros [2] = 500;
+        numeros [3] = 100;
+        numeros [4] = 60 ;
+
+        //impresion en pantalla del vector
+
+        System.out.print("[" + numeros [0] + "]");
+        System.out.print("[" + numeros [1] + "]");
+        System.out.print("[" + numeros [2] + "]");
+        System.out.print("[" + numeros [3] + "]");
+        System.out.print("[" + numeros [4] + "]");
+
+
+        
+
+
 
 
     }
