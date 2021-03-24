005e430eacc820574da26ee56f99c2f4e6623716
marlenescobarl <marlenescobarlima11@gmail.com>
2021-03-23 20:22:42 -0600

    archivo 25, practica de la creacion y hubicacion de una ventana

---
+++ V25ventana.java
@@ -0,0 +1,14 @@
+import javax.swing.*; //lVibreria para la interfaz
+public class V25ventana extends JFrame // extends para la usar herencias JFrame para crear la pantalla
+{
+    public V25ventana() // creacion del objeto
+    {
+        setLayout(null); //para el uso de las coordenadas, 
+    }
+    public static void main (String args [])
+    {
+        V25ventana ventana = new V25ventana (); //creacion de un nuevo objeto 
+        ventana.setBounds(0,0, 300, 500); // hubicacion y tamanno de la ventana
+        ventana.setVisible(true); // para que se visualice la ventana
+    }
+}
