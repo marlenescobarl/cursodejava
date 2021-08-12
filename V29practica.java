37d97c7ca54b094aa251c0e21e3839c2f92fd373
marlenescobarl <marlenescobarlima11@gmail.com>
2021-08-12 03:29:51 -0600

    guardando para prueba uno

--- V29practica.java
+++ V29practica.java
@@ -13,10 +13,34 @@ public class V29practica extends JFrame implements ActionListener {
         label1 = new JLabel ("USUARIO: ");
         label1.setBounds(10, 10, 100, 30);
         add(label1);
-        
+
         //programacion del diseño de la caja de texto
         textfield1 = new JTextField();
+        textfield1.setBounds(120, 17, 150, 20);
+        add(textfield1);
 
-
+        //diseño del boton
+        boton1 = new JButton("ACEPTAR");
+        boton1.setBounds(10, 80, 100, 30);
+        add(boton1);
+        //indicamos que el boton mas adelante tendra un evento
+        boton1.addActionListener(this); 
     }
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == boton1){
+            //obtener el texto que el usuario haya escrito y guardarlo dentro de la variable texto
+            String texto = textfield1.getText();
+
+            //colocar el texto dentro del titulo de la interfaz
+            setTitle(texto);
+        }
+    }
+}
+//programacion de la interfaz
+public static void main (String args[]){
+    V29practica formulario = new V29practica();
+    formulario.setBounds(0,0, 300, 150);
+    formulario.setVisible(true);
+    formulario.setResizable(false);
+    formulario.setLocationRelativeTo(null);
 }
